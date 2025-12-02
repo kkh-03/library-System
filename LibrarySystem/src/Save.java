2a4ed9d1646ff8a0d0735f80c0b2f74fb5f69611
kkh <kkh99480@naver.com>
2025-12-02 21:24:20 +0900

    .

---
+++ LibrarySystem/src/Save.java
@@ -0,0 +1,19 @@
+public class Save {
+    private String title;
+    private String author;
+    private String publisher;
+    private int year;
+
+    public Save(String title, String author, String publisher, int year) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getPublisher() { return publisher; }
+    public int getYear() { return year; }
+}
+
